a1b942b7ad4a019557f72cbd92592af504fa3e3d
LucaBru <lucabrugnera01@gmail.com>
2022-05-26 17:38:51 +0200

    Update

---
+++ Insert Modello.sql
@@ -0,0 +1,13 @@
+INSERT INTO Modello values ('Serie 1 116d', 'BMW', '2004'),
+INSERT INTO Modello values ('Serie 1 118d', 'BMW', '2004'),
+INSERT INTO Modello values ('X1', 'BMW', '2009'),
+INSERT INTO Modello values ('X3', 'BMW', '2003'),
+INSERT INTO Modello values ('X5', 'BMW', '1999'),
+INSERT INTO Modello values ('A1', 'Audi', '2010'),
+INSERT INTO Modello values ('A3', 'Audi', '1996'),
+INSERT INTO Modello values ('A5', 'Audi', '2007'),
+INSERT INTO Modello values ('Classe A', 'Mercedes', '1997'),
+INSERT INTO Modello values ('Classe B', 'Mercedes', '2005')
+INSERT INTO Modello values ('Classe C', 'Mercedes', '1993')
+INSERT INTO Modello values ('Tipo', 'Fiat', '2015'),
+INSERT INTO Modello values ('308', 'Pegeout', '1997')
